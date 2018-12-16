29d2591b07053afdf108be6ae4bdcb5116d4617a
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2018-12-16 21:38:10 +0200

    Work on the dashboard menu.

--- client/src/main/java/ro/lbi/sqliggybank/client/view/register/RegisterController.java
+++ client/src/main/java/ro/lbi/sqliggybank/client/view/register/RegisterController.java
@@ -88,7 +88,11 @@ public class RegisterController {
 	/**
 	 * This method is the default initialize method for an FXML controller class.
 	 * <p>
-	 * It is called <u>before</u> any constructor to set any properties that might be needed in the GUI.
+	 * It is called <u>right after</u> the constructor finished execution and the @FXML annotated fields
+	 * are populated.
+	 *
+	 * <p>
+	 * This method then initializes any attributes needed in the GUI.
 	 */
 	@FXML
 	private void initialize() {
